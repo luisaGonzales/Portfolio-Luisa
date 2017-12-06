7d12855e00ab6f51fb03b825dd22cb83798ab386
luisaGonzales <luisagc711@gmail.com>
2017-12-06 09:53:44 -0500

    Actualizacion

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,13 +4,13 @@ import '../css/main.css';
 
 const Project = ({image, titleProject, descriptionProject, demo, index, github}) => {
     return (
-        <figure className="snip1401">
+        <figure className="projectsList">
         <Image src={image} alt="project" className="portfolioImg" width="310px" height="200px" />
         <figcaption>
           <h3>{titleProject}</h3>
-          <span><a target="_blank" className="btnsPortfolio seeGithub" href={github}>git<i class="fa fa-github" aria-hidden="true"></i></a></span>
+          <span><a target="_blank" className="seeGithub" href={github}>git<i class="fa fa-github" aria-hidden="true"></i></a></span>
         </figcaption>
-        <span><a target="_blank" className="btnsPortfolio" href={demo}><i className="fa fa-eye" aria-hidden="true"></i></a></span>      
+        <span><a target="_blank" href={demo}><i className="fa fa-eye" aria-hidden="true"></i></a></span>      
       </figure>
     );
 }
@@ -19,7 +19,7 @@ export const Portfolio = ({title, info}) => {
     console.log(info);
     const list = info.projects.imgs.map((img, index)=> {
         return (
-            <Col lg={4} md={12} sm={12} xs={12} >
+            <Col lg={4} md={4} sm={6} xs={12} className="allProjects" >
                 <Project key={index} github={info.projects.github[index]} index={index + 1} image={img} titleProject={info.projects.titles[index]} demo={info.projects.demo[index]}/>
             </Col>  
         );
