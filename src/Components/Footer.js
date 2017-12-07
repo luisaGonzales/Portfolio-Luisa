9a73abb5ac2acf265a7a774e85b1545ed1342b07
luisaGonzales <luisagc711@gmail.com>
2017-12-07 09:18:55 -0500

    Updating

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,10 @@ export const Footer = () => {
     return(
         <Row className="footer">
             <Col xs={12} lg={12} sm={12} md={12}>
-                <p className="pull-right"> Made with Hope</p>
+                <div className="pull-right"> MADE WITH HOPE.</div>
+            </Col>
+            <Col xs={12} lg={12} sm={12} md={12}>
+                <div className="pull-right"> 2017</div>
             </Col>
         </Row>
     );
